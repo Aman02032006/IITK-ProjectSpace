449977a0088d6f4c74bc82b3b2e2cae14697aa2a
Aman Srivastava <aman02032006@gmail.com>
2026-03-26 19:38:57 +0530

    Fixed User Search cards

--- frontend/src/app/components/cards/UserCard.tsx
+++ frontend/src/app/components/cards/UserCard.tsx
@@ -1,4 +1,4 @@
-import OverflowTooltip from "@/app/components/cards/OverflowTooltip"
+ import OverflowTooltip from "@/app/components/cards/OverflowTooltip"
 
 interface UserCardProps {
   name: string
@@ -10,14 +10,7 @@ interface UserCardProps {
 
 
 
-export default function UserCard({
-  name,
-  designation,
-  department,
-  organisation,
-  image
-}: UserCardProps) {
-
+export default function UserCard({ name, designation, department, organisation, image}: UserCardProps) {
   return (
     <div className="bg-white border border-gray-200 rounded-xl p-4 shadow-[0_0_10px_rgba(0,0,0,0.1)] hover:shadow-[0_0_15px_rgba(0,0,0,0.15)] transition-shadow duration-200 flex items-center gap-4 w-full max-w-sm">
       <div className="w-16 h-16 rounded-md bg-muted flex-shrink-0 overflow-hidden">
